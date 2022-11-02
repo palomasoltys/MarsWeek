673681b3b2f15098b317e02c9db9fe1cb9f6d3e4
palomasoltys <pfs601@gmail.com>
2022-11-02 11:52:03 -0400

    Add GuessingGame line 83

--- src/Mars.java
+++ src/Mars.java
@@ -20,6 +20,7 @@ public class Mars {
             System.out.println("ERROR!!! Flight plan already set. Landing on the Plain");
         }
         landing = landingCheck(100);
+        new GuessingGame();
 
     }
 
