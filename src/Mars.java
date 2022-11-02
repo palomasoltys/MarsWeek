c238f938acb25c34e1388a2c48d536bb60f6cd1d
palomasoltys <pfs601@gmail.com>
2022-11-02 10:42:53 -0400

    Finish homework Mars.java

--- src/Mars.java
+++ src/Mars.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class Mars {
+public class Mars {
+    public static void main(String[] args) throws InterruptedException {
+
+        String colonyName = "Dragonstone";
+        int shipPopulation = 300;
+        double shipFood = 4000.00;
+        boolean landing = true;
+
+        shipFood = (shipFood - (shipPopulation * 0.75)) * 2;
+        System.out.println("The amount of food left after landing is " + shipFood + ".");
+
+        shipFood = (shipFood * 0.5) + shipFood;
+        shipPopulation += 5;
+
+        String landingLocation = "The Plain";
+
+        if (landingLocation.equalsIgnoreCase("The Plain")) {
+            System.out.println("Bbzzz Landing on the Plain");
+        } else {
+            System.out.println("ERROR!!! Flight plan already set. Landing on the Plain");
+        }
+        landing = landingCheck(100);
+
+    }
+
+    public static boolean landingCheck(int loops) throws InterruptedException {
+        for (int i = 0; i <= loops ; i++) {
+            if(i % 3 == 0 && i % 5 == 0) {
+                System.out.println("Keep Center");
+            } else if(i % 5 == 0) {
+                System.out.println("Right");
+            } else if(i % 3 == 0) {
+                System.out.println("Left");
+            } else {
+                System.out.println("Calculating");
+            }
+            Thread.sleep(250);
+
+        }
+        System.out.println("Landed");
+        return false;
+    }
 }
