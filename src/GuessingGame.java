268da66a1465e8218eba436273ebbc8267b5a05f
palomasoltys <pfs601@gmail.com>
2022-11-02 11:07:54 -0400

    Create while loop and check if user guess the right number

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -16,7 +16,33 @@ public class GuessingGame {
         Random rand = new Random();
         int number = rand.nextInt(100);
 
+        System.out.println("Try to guess my number.");
 
+        while(!correctGuess) {
+            increment++;
+            try {
+                guess = input.nextInt();
+
+
+            } catch(Exception e) {
+                String badInput = input.next();
+                System.out.println("That is not an integer, try again.");
+                continue;
+
+            }
+            if(guess > 100 || guess < 1) {
+                System.out.println("Your guess is out of range. Please, try again.");
+            } else if (guess > number) {
+                System.out.println("Your guess is too high. Please, try again");
+            } else if (guess < number) {
+                System.out.println("Your guess is too low. Please, try again");
+            } else {
+                System.out.println("Well done, " + name + "! You found my number in " + increment + " tries!");
+                correctGuess = true;
+            }
+
+
+        }
 
     }
 }
